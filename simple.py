4703259fec98cf96fc174ab6548f138fe6c1b29a
rejuvyesh <mail@rejuvyesh.com>
2016-07-11 23:57:51 -0700

    add arguments

--- simple.py
+++ simple.py
@@ -4,61 +4,108 @@
 #
 # Created: Wednesday, July  6 2016 by rejuvyesh <mail@rejuvyesh.com>
 #
-from __future__ import print_function
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
 
+import argparse
+import json
 import sys
 sys.path.append('../rltools/')
 
 import numpy as np
 import tensorflow as tf
 
-import rltools.log
 import gym
 import rltools.algos
-from rltools.baseline import LinearFeatureBaseline, MLPBaseline
+import rltools.log
+import rltools.util
+from madrl_environments.pursuit.centralized_pursuit_evade import \
+    CentralizedPursuitEvade
+from madrl_environments.pursuit.utils import TwoDMaps
+from rltools.baseline import LinearFeatureBaseline, MLPBaseline, ZeroBaseline
 from rltools.categorical_policy import CategoricalMLPPolicy
 
-from madrl_environments.pursuit.centralized_pursuit_evade import CentralizedPursuitEvade
-from madrl_environments.pursuit.utils import TwoDMaps
 
-def main():
-    env = CentralizedPursuitEvade(TwoDMaps.rectangle_map(10,10), n_evaders=5, n_pursuers=2, obs_range=3, n_catch=2)
-    discount = 0.95
-    hidden_spec = '''[
+
+SIMPLE_POLICY_ARCH = '''[
         {"type": "fc", "n": 128},
         {"type": "nonlin", "func": "tanh"},
         {"type": "fc", "n": 128},
         {"type": "nonlin", "func": "tanh"}
     ]
     '''
-    val_spec = '''[
+TINY_VAL_ARCH = '''[
         {"type": "fc", "n": 32},
         {"type": "nonlin", "func": "relu"},
         {"type": "fc", "n": 32},
         {"type": "nonlin", "func": "relu"}
     ]
     '''
+SIMPLE_VAL_ARCH = '''[
+        {"type": "fc", "n": 128},
+        {"type": "nonlin", "func": "relu"},
+        {"type": "fc", "n": 128},
+        {"type": "nonlin", "func": "relu"}
+    ]
+    '''
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--discount', type=float, default=0.95)
+    parser.add_argument('--gae_lambda', type=float, default=0.99)
+    parser.add_argument('--max_traj_len', type=int, default=200)
+    parser.add_argument('--rectangle', type=str, default='10,10')
+    parser.add_argument('--n_evaders', type=int, default=5)
+    parser.add_argument('--n_pursuers', type=int, default=2)
+    parser.add_argument('--obs_range', type=int, default=3)
+    parser.add_argument('--n_catch', type=int, default=2)
+    parser.add_argument('--policy_hidden_spec', type=str, default=SIMPLE_POLICY_ARCH)
+    parser.add_argument('--baseline_type', type=str, default='mlp')
+    parser.add_argument('--baseline_hidden_spec', type=str, default=SIMPLE_VAL_ARCH)
+    parser.add_argument('--max_kl', type=float, default=0.01)
+    parser.add_argument('--vf_max_kl', type=float, default=0.01)
+    parser.add_argument('--vf_cg_damping', type=float, default=0.01)
+    parser.add_argument('--save_freq', type=int, default=20)
+    parser.add_argument('--log', type=str, required=False)
+    args = parser.parse_args()
+    env = CentralizedPursuitEvade(TwoDMaps.rectangle_map(*map(int, args.rectangle.split(','))),
+                                  n_evaders=args.n_evaders,
+                                  n_pursuers=args.n_pursuers,
+                                  obs_range=args.obs_range,
+                                  n_catch=args.n_catch)
     tboard_dir = '/tmp/madrl_tb'
-    policy = CategoricalMLPPolicy(env.observation_space, env.action_space, hidden_spec=hidden_spec, enable_obsnorm=True, tblog=tboard_dir, varscope_name='catmlp_policy')
-    baseline = LinearFeatureBaseline(env.observation_space, enable_obsnorm=True)
-    #baseline = MLPBaseline(env.observation_space, val_spec, True, True, max_kl=0.01, damping=1e-2, time_scale=1., varscope_name='mlp_baseline')
-    step_func = rltools.algos.TRPO(max_kl=0.01)
+    policy = CategoricalMLPPolicy(env.observation_space, env.action_space,
+                                  hidden_spec=args.policy_hidden_spec,
+                                  enable_obsnorm=True,
+                                  tblog=tboard_dir, varscope_name='catmlp_policy')
+
+    if args.baseline_type == 'linear':
+        baseline = LinearFeatureBaseline(env.observation_space, enable_obsnorm=True)
+    elif args.baseline_type == 'mlp':
+        baseline = MLPBaseline(env.observation_space, args.baseline_hidden_spec,
+                               True, True, max_kl=args.vf_max_kl, damping=args.vf_cg_damping,
+                               time_scale=1./args.max_traj_len, varscope_name='mlp_baseline')
+    else:
+        baseline = ZeroBaseline(env.observation_space)
+
+    step_func = rltools.algos.TRPO(max_kl=args.max_kl)
     popt = rltools.algos.SamplingPolicyOptimizer(
         env=env,
         policy=policy,
         baseline=baseline,
         step_func=step_func,
-        discount=discount,
+        discount=args.discount,
+        gae_lambda=args.gae_lambda,
         batch_size=32
     )
-    save_file = '/tmp/s.h5'
-    
-    log_f = rltools.log.TrainingLog(save_file, [])
+    argstr = json.dumps(vars(args), separators=(',', ':'), indent=2)
+    rltools.util.header(argstr)
+    log_f = rltools.log.TrainingLog(args.log, [('args', argstr)])
+
     with tf.Session() as sess:
         sess.run(tf.initialize_all_variables())
-        popt.train(sess, log_f)
+        popt.train(sess, log_f, args.save_freq)
 
-        
+
 if __name__ == '__main__':
     main()
