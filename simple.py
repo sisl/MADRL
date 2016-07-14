f96b87c9e199bb5f163c2e68c71649586c64ed6f
rejuvyesh <mail@rejuvyesh.com>
2016-07-13 19:56:08 -0700

    sampler arguments

--- simple.py
+++ simple.py
@@ -18,6 +18,7 @@ import gym
 import rltools.algos
 import rltools.log
 import rltools.util
+from rltools.sampler import SimpleSampler, ImportanceWeightedSampler
 from madrl_environments.pursuit.centralized_pursuit_evade import \
     CentralizedPursuitEvade
 from madrl_environments.pursuit.utils import TwoDMaps
@@ -42,9 +43,9 @@ TINY_VAL_ARCH = '''[
     '''
 SIMPLE_VAL_ARCH = '''[
         {"type": "fc", "n": 128},
-        {"type": "nonlin", "func": "relu"},
+        {"type": "nonlin", "func": "tanh"},
         {"type": "fc", "n": 128},
-        {"type": "nonlin", "func": "relu"}
+        {"type": "nonlin", "func": "tanh"}
     ]
     '''
 
@@ -53,19 +54,35 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--discount', type=float, default=0.95)
     parser.add_argument('--gae_lambda', type=float, default=0.99)
+
+    parser.add_argument('--sampler', type=str, default='simple')
     parser.add_argument('--max_traj_len', type=int, default=200)
     parser.add_argument('--adaptive_batch', action='store_true', default=False)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--min_batch_size', type=int, default=4)
+    parser.add_argument('--max_batch_size', type=int, default=64)
+    parser.add_argument('--batch_rate', type=int, default=40)
+    parser.add_argument('--is_n_backtrack', type=int, default=1)
+    parser.add_argument('--is_randomize_draw', action='store_true', default=False)
+    parser.add_argument('--is_n_pretrain', type=int, default=0)
+    parser.add_argument('--is_skip_is', action='store_true', default=False)
+    parser.add_argument('--is_max_is_ratio', type=float, default=0)
+
     parser.add_argument('--rectangle', type=str, default='10,10')
     parser.add_argument('--n_evaders', type=int, default=5)
     parser.add_argument('--n_pursuers', type=int, default=2)
     parser.add_argument('--obs_range', type=int, default=3)
     parser.add_argument('--n_catch', type=int, default=2)
+
     parser.add_argument('--policy_hidden_spec', type=str, default=SIMPLE_POLICY_ARCH)
+
     parser.add_argument('--baseline_type', type=str, default='mlp')
     parser.add_argument('--baseline_hidden_spec', type=str, default=SIMPLE_VAL_ARCH)
+
     parser.add_argument('--max_kl', type=float, default=0.01)
     parser.add_argument('--vf_max_kl', type=float, default=0.01)
     parser.add_argument('--vf_cg_damping', type=float, default=0.01)
+
     parser.add_argument('--save_freq', type=int, default=20)
     parser.add_argument('--log', type=str, required=False)
     args = parser.parse_args()
@@ -89,6 +106,29 @@ def main():
     else:
         baseline = ZeroBaseline(env.observation_space)
 
+    if args.sampler == 'simple':
+        sampler_cls = SimpleSampler
+        sampler_args = dict(max_traj_len=args.max_traj_len,
+                            batch_size=args.batch_size,
+                            min_batch_size=args.min_batch_size,
+                            max_batch_size=args.max_batch_size,
+                            batch_rate=args.batch_rate,
+                            adaptive=args.adaptive_batch)
+    elif args.sampler == 'imp':
+        sampler_cls = ImportanceWeightedSampler
+        sampler_args = dict(max_traj_len=args.max_traj_len,
+                            batch_size=args.batch_size,
+                            min_batch_size=args.min_batch_size,
+                            max_batch_size=args.max_batch_size,
+                            batch_rate=args.batch_rate,
+                            adaptive=args.adaptive_batch,
+                            n_backtrack=args.is_n_backtrack,
+                            randomize_draw=args.is_randomize_draw,
+                            n_pretrain=args.is_n_pretrain,
+                            skip_is=args.is_skip_is,
+                            max_is_ratio=args.is_max_is_ratio)
+    else:
+        raise NotImplementedError()
     step_func = rltools.algos.TRPO(max_kl=args.max_kl)
     popt = rltools.algos.SamplingPolicyOptimizer(
         env=env,
@@ -97,8 +137,8 @@ def main():
         step_func=step_func,
         discount=args.discount,
         gae_lambda=args.gae_lambda,
-        batch_size=32,
-        adaptive_batch=args.adaptive_batch
+        sampler_cls=sampler_cls,
+        sampler_args=sampler_args,
     )
     argstr = json.dumps(vars(args), separators=(',', ':'), indent=2)
     rltools.util.header(argstr)
