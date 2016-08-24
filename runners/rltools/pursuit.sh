158a749fe484852638f78bbfb5cef21c64b96678
Maxim Egorov <maxgegorov@gmail.com>
2016-08-24 10:01:58 -0700

    script update

--- runners/rltools/pursuit.sh
+++ runners/rltools/pursuit.sh
@@ -1,4 +1,2 @@
-python run_pursuit.py --log pursuit_med_32.h5 --n_iter 1000 --n_evaders 50 --n_pursuers 30 --control decentralized
---n_timesteps 25000 --obs_range 11 --sample_maps --sampler simple --map_file ../maps/map_pool32.npy --policy_hidden_spec
-SIMPLE_POLICY_ARCH --baseline_hidden_spec SIMPLE_POLICY_ARCH --sampler_workers 4 --flatten
+python run_pursuit.py --log pursuit_med_32.h5 --n_iter 1000 --n_evaders 50 --n_pursuers 30 --control decentralized --n_timesteps 25000 --obs_range 11 --sample_maps --sampler simple --map_file ../maps/map_pool32.npy --policy_hidden_spec MED_POLICY_ARCH --baseline_hidden_spec MED_POLICY_ARCH --sampler_workers 4 --flatten
 
