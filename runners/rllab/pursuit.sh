af10da8ac380f0546846b43cf7983d991e197f83
Max Egorov <maxgegorov@gmail.com>
2016-08-30 10:19:32 -0700

    updated run scripts

--- runners/rllab/pursuit.sh
+++ runners/rllab/pursuit.sh
@@ -1,1 +1,1 @@
-python run_pursuit.py --log_dir pursuit_gru32_test --n_iter 500 --n_evaders 5 --n_pursuers 4 --control decentralized --n_timesteps 10000 --obs_range 5 --sample_maps --map_file ../maps/map_pool32.npy --policy_hidden_sizes 32,32 --flatten
+python run_pursuit.py --log_dir gru_test --n_iter 500 --n_evaders 20 --n_pursuers 20 --control decentralized --n_timesteps 10000 --obs_range 5 --sample_maps --map_file ../maps/map_pool32.npy --policy_hidden_sizes 8 --flatten --recurrent --sampler_workers 1 --surround --discount 0.99
