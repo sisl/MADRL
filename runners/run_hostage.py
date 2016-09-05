a47f23317563ba2afd024886408015ce035a5742
rejuvyesh <mail@rejuvyesh.com>
2016-09-05 11:42:07 -0700

    Change defaults

--- runners/run_hostage.py
+++ runners/run_hostage.py
@@ -7,16 +7,16 @@
 from runners import RunnerParser, comma_sep_ints
 from runners.rurllab import RLLabRunner
 from runners.rurltools import RLToolsRunner
-from madrl_environments.hostage import ContinuousHostage
+from madrl_environments.hostage import ContinuousHostageWorld
 from madrl_environments import StandardizedEnv, ObservationBuffer
 
 # yapf: disable
 ENV_OPTIONS = [
-    ('n_good', int, 4, ''),
-    ('n_hostages', int, 12, ''),
-    ('n_bad', int, 12, ''),
+    ('n_good', int, 8, ''),
+    ('n_hostages', int, 16, ''),
+    ('n_bad', int, 16, ''),
     ('n_coop_save', int, 4, ''),
-    ('n_coop_avoid', int, 1, ''),
+    ('n_coop_avoid', int, 2, ''),
     ('radius', float, 0.015, ''),
     ('bomb_radius', float, 0.03, ''),
     ('key_loc', comma_sep_ints, None, ''),
@@ -26,6 +26,7 @@ ENV_OPTIONS = [
     ('save_reward', float, 10, ''),
     ('hit_reward', float, -1, ''),
     ('encounter_reward', float, 0.01, ''),
+    ('not_saved_reward', float, -3, ''),
     ('bomb_reward', float, -20, ''),
     ('control_penalty', float, -0.1, ''),
     ('reward_mech', str, 'local', ''),
@@ -37,23 +38,23 @@ def main(parser):
     mode = parser._mode
     args = parser.args
 
-    env = ContinuousHostage(n_good=args.n_good,
-                            n_hostages=args.n_hostages,
-                            n_bad=args.n_bad,
-                            n_coop_save=args.n_coop_save,
-                            n_coop_avoid=args.n_coop_avoid,
-                            radius=args.radius,
-                            key_loc=args.key_loc,
-                            bad_speed=args.bad_speed,
-                            n_sensors=args.n_sensors,
-                            sensor_range=args.sensor_range,
-                            save_reward=args.save_reward,
-                            hit_reward=args.hit_reward,
-                            encounter_reward=args.encounter_reward,
-                            bomb_reward=args.bomb_reward,
-                            bomb_radius=args.bomb_radius,
-                            control_penalty=args.control_penalty,
-                            reward_mech=args.reward_mech,)
+    env = ContinuousHostageWorld(n_good=args.n_good,
+                                 n_hostages=args.n_hostages,
+                                 n_bad=args.n_bad,
+                                 n_coop_save=args.n_coop_save,
+                                 n_coop_avoid=args.n_coop_avoid,
+                                 radius=args.radius,
+                                 key_loc=args.key_loc,
+                                 bad_speed=args.bad_speed,
+                                 n_sensors=args.n_sensors,
+                                 sensor_range=args.sensor_range,
+                                 save_reward=args.save_reward,
+                                 hit_reward=args.hit_reward,
+                                 encounter_reward=args.encounter_reward,
+                                 bomb_reward=args.bomb_reward,
+                                 bomb_radius=args.bomb_radius,
+                                 control_penalty=args.control_penalty,
+                                 reward_mech=args.reward_mech,)
 
     if args.buffer_size > 1:
         env = ObservationBuffer(env, args.buffer_size)
